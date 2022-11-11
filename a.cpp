c95d00cf5f40920ec44d307cbfc54a719a1fe4cc
Venom-61 <subhendukarmakar61@gmail.com>
2022-11-11 20:36:57 +0530

    Immobile Knight

---
+++ a.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+// A. Immobile Knight
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    cout << n/2 + 1 << " " << m/2 + 1 << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}
